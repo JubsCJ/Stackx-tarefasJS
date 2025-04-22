9dd690d681f5c72702ef2a450de1003169ed14d6
JubsCJ <carlosjuniorbot@gmail.com>
2025-04-21 21:12:03 -0300

    tarefa 1 JS

---
+++ tarefa1.js
@@ -0,0 +1,13 @@
+/*1. Concatenação de Arrays
+Crie dois arrays, um com nomes de cidades brasileiras e outro com nomes de cidades estrangeiras.
+Utilize o método concatLinks to an external site. para unir esses dois arrays em um único. Exiba o array resultante no console.*/
+
+let cidadesBrasileiras = ["Porto Alegre", "Canoas", "Cachoeirinha"];
+
+let cidadesJapao = ["Toquio", "Osaka", "Quioto"];
+
+cidadesBrasileiras.concat(cidadesJapao);
+
+const cindadesUnidas = cidadesBrasileiras.concat(cidadesJapao)
+
+console.log(cindadesUnidas)
